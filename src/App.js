aeaf5bf5b51646093cb57a89a3f6230f5942930f
Jacob0312 <jaenk0312@gmail.com>
2022-05-11 00:54:08 +0200

    Practicing Todolist

--- src/App.js
+++ src/App.js
@@ -11,15 +11,50 @@ function Hello() {
 }
 
 function App() {
-	const [show, showControll] = useState(true);
-	const onClick = () => {
-		showControll((prev) => !prev);
+	const [toDo, setTodo] = useState("");
+	const [toDos, setToDos] = useState([]);
+
+	const onChange = (event) => {
+		setTodo(event.target.value);
+	};
+	const onSubmit = (event) => {
+		event.preventDefault();
+
+		if ({ toDo } === "") {
+			return;
+		} else {
+			setToDos((currentArray) => [toDo, ...currentArray]);
+			console.log(toDos);
+			setTodo("");
+		}
+	};
+
+	const onClick = (event) => {
+		const li = event.target.parentElement;
+
+		li.remove();
 	};
 
 	return (
 		<div>
-			{show ? <Hello /> : null}
-			<button onClick={onClick}>{show ? "Hide" : "Show"}</button>
+			<h1>My To toDos ({toDos.length})</h1>
+			<ul>
+				{toDos.map((item, index) => (
+					<li key={index}>
+						{item} <button onClick={onClick}>❌</button>
+					</li>
+				))}
+			</ul>
+
+			<form onSubmit={onSubmit}>
+				<input
+					value={toDo}
+					type="text"
+					placeholder="Put your todos"
+					onChange={onChange}
+				></input>
+				<button>Add To Do</button>
+			</form>
 		</div>
 	);
 }
