3a2f52d728a10a5196adc4fc3a233f3b84765f89
Jacob0312 <jaenk0312@gmail.com>
2022-05-10 19:24:53 +0200

    useEffect / Cleanup

--- src/App.js
+++ src/App.js
@@ -1,35 +1,25 @@
 import Button from "./button.js";
 import styles from "./Title.module.css";
 import { useState, useEffect } from "react";
-function App() {
-	const [click, setClick] = useState(0);
-	const onClick = () => setClick((prev) => prev + 1);
 
-	const [search, setSearch] = useState("");
-	const onSearch = (event) => setSearch(event.target.value);
-
-	const runCommand = () => console.log("I run only once!");
-	useEffect(runCommand, []);
+function Hello() {
 	useEffect(() => {
-		console.log("I run all the times");
-	}, [click]);
+		console.log("Hello!");
+		return () => console.log("Bye");
+	}, []);
+	return <h1>Hello!</h1>;
+}
 
-	const ifSearch = () => {
-		if ([search] !== "" && search.length > 5) {
-			console.log(`Searching for ${search}`);
-		}
+function App() {
+	const [show, showControll] = useState(true);
+	const onClick = () => {
+		showControll((prev) => !prev);
 	};
-	useEffect(ifSearch, [search]);
+
 	return (
 		<div>
-			<input
-				type="text"
-				placeholder="Search"
-				value={search}
-				onChange={onSearch}
-			></input>
-			<h1 className={styles.title}>Clicked: {click} </h1>
-			<Button text={"continue"} onClick={onClick} />
+			{show ? <Hello /> : null}
+			<button onClick={onClick}>{show ? "Hide" : "Show"}</button>
 		</div>
 	);
 }
