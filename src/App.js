5c3b6d6e92de7e6de96ec1a92d8df9fd4b3ea75b
Jacob0312 <jaenk0312@gmail.com>
2022-05-10 18:22:22 +0200

    React learning

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Button from "./button.js";
+import styles from "./Title.module.css";
+import { useState, useEffect } from "react";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [click, setClick] = useState(0);
+	const onClick = () => setClick((prev) => prev + 1);
+
+	const [search, setSearch] = useState("");
+	const onSearch = (event) => setSearch(event.target.value);
+
+	const runCommand = () => console.log("I run only once!");
+	useEffect(runCommand, []);
+	useEffect(() => {
+		console.log("I run all the times");
+	}, [click]);
+
+	const ifSearch = () => {
+		if ([search] !== "" && search.length > 5) {
+			console.log(`Searching for ${search}`);
+		}
+	};
+	useEffect(ifSearch, [search]);
+	return (
+		<div>
+			<input
+				type="text"
+				placeholder="Search"
+				value={search}
+				onChange={onSearch}
+			></input>
+			<h1 className={styles.title}>Clicked: {click} </h1>
+			<Button text={"continue"} onClick={onClick} />
+		</div>
+	);
 }
 
 export default App;
