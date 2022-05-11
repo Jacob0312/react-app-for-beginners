4b99d6ac8559cafc642dc9e0ab9b9fc08299347f
Jacob0312 <jaenk0312@gmail.com>
2022-05-11 02:59:57 +0200

    Huge Change in Coin Tracker!
    Used regex / map / if / else if

--- src/App.js
+++ src/App.js
@@ -1,62 +1,74 @@
-import Button from "./button.js";
-import styles from "./Title.module.css";
 import { useState, useEffect } from "react";
 
-function Hello() {
-	useEffect(() => {
-		console.log("Hello!");
-		return () => console.log("Bye");
-	}, []);
-	return <h1>Hello!</h1>;
-}
-
 function App() {
-	const [toDo, setTodo] = useState("");
-	const [toDos, setToDos] = useState([]);
+	const [loading, setLoading] = useState(true);
 
-	const onChange = (event) => {
-		setTodo(event.target.value);
-	};
-	const onSubmit = (event) => {
-		event.preventDefault();
+	const [coins, setCoins] = useState([]);
+	useEffect(() => {
+		fetch("https://api.coinpaprika.com/v1/tickers")
+			.then((response) => response.json())
+			.then((json) => {
+				setCoins(json);
+				setLoading(false);
+			});
+	}, []);
 
-		if ({ toDo } === "") {
-			return;
-		} else {
-			setToDos((currentArray) => [toDo, ...currentArray]);
-			console.log(toDos);
-			setTodo("");
-		}
-	};
-
-	const onClick = (event) => {
-		const li = event.target.parentElement;
-
-		li.remove();
+	const [money, setMoney] = useState(0);
+	const onChange = (event) => setMoney(event.target.value);
+	const [select, setSelect] = useState(false);
+	const [price, setPrice] = useState(0);
+	const [name, setName] = useState("");
+	const regex = /[+-]?\d+(\.\d+)?/g;
+	const onChangeOp = (event) => {
+		setSelect(true);
+		setPrice(event.target.value.match(regex));
+		setName(event.target.value.replace(/ .*/, ""));
 	};
 
 	return (
 		<div>
-			<h1>My To toDos ({toDos.length})</h1>
-			<ul>
-				{toDos.map((item, index) => (
-					<li key={index}>
-						{item} <button onClick={onClick}>❌</button>
-					</li>
-				))}
-			</ul>
+			{loading ? <h1>The Coins!</h1> : <h1>{coins.length} loaded</h1>}
+			{loading ? (
+				<h1>Loading...</h1>
+			) : (
+				<select onChange={onChangeOp}>
+					{coins.map((coin) => (
+						<option>
+							{coin.name} ({coin.symbol}): ${coin.quotes.USD.price} USD
+						</option>
+					))}
+				</select>
+			)}
 
-			<form onSubmit={onSubmit}>
-				<input
-					value={toDo}
-					type="text"
-					placeholder="Put your todos"
-					onChange={onChange}
-				></input>
-				<button>Add To Do</button>
-			</form>
+			{loading === false && select ? (
+				<form>
+					<label htmlFor="USD">The USD you have: </label>
+					<input
+						id="USD"
+						placeholder="Input your money in USD"
+						type="number"
+						onChange={onChange}
+						value={money}
+					></input>
+				</form>
+			) : loading === true && select === false ? null : (
+				<h3>🙄Select the coins first!</h3>
+			)}
+
+			{loading === false && money != 0 ? (
+				<form>
+					<label>Coins you can convert: </label>
+					<input
+						placeholder="result"
+						disabled
+						id="converter"
+						value={money / Math.floor(price) + " " + name}
+					></input>
+				</form>
+			) : loading === true && money === 0 ? null : (
+				<h3>🤔Fill in the USD first</h3>
+			)}
 		</div>
 	);
 }
-
 export default App;
