6f4d9c5063e40441011ba1ed0f3cec0710a9eee2
Jacob0312 <jaenk0312@gmail.com>
2022-05-11 03:53:44 +0200

    Movie fetch using async / await

--- src/App.js
+++ src/App.js
@@ -1,72 +1,38 @@
 import { useState, useEffect } from "react";
 
 function App() {
+	const [movies, setMovies] = useState([]);
 	const [loading, setLoading] = useState(true);
-
-	const [coins, setCoins] = useState([]);
-	useEffect(() => {
-		fetch("https://api.coinpaprika.com/v1/tickers")
-			.then((response) => response.json())
-			.then((json) => {
-				setCoins(json);
-				setLoading(false);
-			});
-	}, []);
-
-	const [money, setMoney] = useState(0);
-	const onChange = (event) => setMoney(event.target.value);
-	const [select, setSelect] = useState(false);
-	const [price, setPrice] = useState(0);
-	const [name, setName] = useState("");
-	const regex = /[+-]?\d+(\.\d+)?/g;
-	const onChangeOp = (event) => {
-		setSelect(true);
-		setPrice(event.target.value.match(regex));
-		setName(event.target.value.replace(/ .*/, ""));
+	const getMovies = async () => {
+		const json = await (
+			await fetch(
+				`https://yts.mx/api/v2/list_movies.json?minimum_rating=9.0&sort_by=year`
+			)
+		).json();
+		setMovies(json.data.movies);
+		setLoading(false);
 	};
-
+	useEffect(() => {
+		getMovies();
+	}, []);
 	return (
 		<div>
-			{loading ? <h1>The Coins!</h1> : <h1>{coins.length} loaded</h1>}
 			{loading ? (
 				<h1>Loading...</h1>
 			) : (
-				<select onChange={onChangeOp}>
-					{coins.map((coin) => (
-						<option>
-							{coin.name} ({coin.symbol}): ${coin.quotes.USD.price} USD
-						</option>
+				<div>
+					{movies.map((movie) => (
+						<div key={movie.id}>
+							<h2>{movie.title}</h2> <h3>rating: {movie.rating}</h3>
+							<img src={movie.medium_cover_image} />{" "}
+							<ul>
+								{movie.genres.map((g) => (
+									<li key={g}>{g}</li>
+								))}
+							</ul>
+						</div>
 					))}
-				</select>
-			)}
-
-			{loading === false && select ? (
-				<form>
-					<label htmlFor="USD">The USD you have: </label>
-					<input
-						id="USD"
-						placeholder="Input your money in USD"
-						type="number"
-						onChange={onChange}
-						value={money}
-					></input>
-				</form>
-			) : loading === true && select === false ? null : (
-				<h3>🙄Select the coins first!</h3>
-			)}
-
-			{loading === false && money != 0 ? (
-				<form>
-					<label>Coins you can convert: </label>
-					<input
-						placeholder="result"
-						disabled
-						id="converter"
-						value={money / Math.floor(price) + " " + name}
-					></input>
-				</form>
-			) : loading === true && money === 0 ? null : (
-				<h3>🤔Fill in the USD first</h3>
+				</div>
 			)}
 		</div>
 	);
